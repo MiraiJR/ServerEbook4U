d5225cf886f966c1553334c0089812b4231242b7
20120471 <truongvanhao159@gmail.com>
2022-12-26 16:28:46 +0700

    [26/12/22] [hao] fix

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -10,7 +10,7 @@ const adminController = require("../app/controllers/AdminController.js")
 const reportController = require("../app/controllers/ReportController.js")
 const notifyController = require("../app/controllers/NotifyController.js")
 
-router.get("/user/:id/:status", verifyToken, verifyRole, adminController.banOrUnbanAccount)
+router.put("/user/:id/:status", verifyToken, verifyRole, adminController.banOrUnbanAccount)
 
 // interact with report
 router.get("/report/all", verifyToken, verifyRole, reportController.getAllReport)
